f7474314eb6d597a3e42129986b49a717b316ea4
Romuald Brillout <git@brillout.com>
2021-06-13 13:51:28 +0200

    wip

--- packages/vue-stem/cli/index.ts
+++ packages/vue-stem/cli/index.ts
@@ -1,1 +1,30 @@
-console.log('h1')
+import { cac } from "cac";
+import { dev } from "../dev";
+const pkg = require("../../package.json");
+
+const cli = cac(pkg.name);
+
+cli
+  .command("dev")
+  //.option('--partial', 'allow only a subset of pages to be pre-rendered')
+  .action(async (options) => {
+    //const { partial, clientRouter, base, root } = options
+    dev();
+  });
+
+cli.on("command:*", () => {
+  console.error("Invalid command: %s", cli.args.join(" "));
+});
+
+cli.help();
+cli.version(require("../../package.json").version);
+
+cli.parse(
+  process.argv.length === 2 ? [...process.argv, "--help"] : process.argv
+);
+
+process.on("unhandledRejection", (rejectValue) => {
+  throw rejectValue;
+});
+
+console.log("h1");
---
+++ packages/vue-stem/dev.ts
@@ -0,0 +1,5 @@
+export { dev };
+
+function dev() {
+  console.log("dev");
+}
